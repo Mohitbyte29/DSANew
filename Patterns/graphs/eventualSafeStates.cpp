e79c2b7ababa89fe6a0753c6db5e22654ab38752
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-06-12 21:02:18 +0530

    eventual safe states by BFS

--- Patterns/graphs/eventualSafeStates.cpp
+++ Patterns/graphs/eventualSafeStates.cpp
@@ -38,3 +38,37 @@ public:
         return safenodes;
     };
 };
+
+//* BFS
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int n = adj.size();
+        vector<int> safenodes;
+        vector<int> indegree(n, 0);
+        queue<int> q;
+        vector<vector<int>> adjRev(n);
+        for(int i = 0; i<n; i++){
+            for(auto it : adj[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safenodes.push_back(node);
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        sort(safenodes.begin(), safenodes.end());
+        return safenodes;
+    };
+};
