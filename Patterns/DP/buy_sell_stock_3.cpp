4925f74aa50eb0684aae615df46a59b1b664191b
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-07-23 14:22:25 +0530

    buy and sell stocks for 2 transactions

---
+++ Patterns/DP/buy_sell_stock_3.cpp
@@ -0,0 +1,40 @@
+// Memoization
+class Solution {
+ int f(int i, int buy, vector<int>& prices, int n, int cap, vector<vector<vector<int>>>& dp){
+        if(i == n) return 0;
+        if(cap == 0) return 0;
+        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+        if(buy){
+            dp[i][buy][cap] = max(-prices[i] + f(i + 1, 0, prices, n, cap, dp), 0 + f(i + 1, 1, prices, n, cap, dp));
+        }
+        else{
+            dp[i][buy][cap] = max(prices[i] + f(i + 1, 1, prices, n, cap - 1, dp), 0 + f(i + 1, 0, prices, n, cap, dp));
+        }
+        return dp[i][buy][cap];
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int> (3, -1)));
+        return f(0, 1, prices, n, 2, dp);
+    }
+};
+
+// Tabulation
+class Solution {
+ 
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+         vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int> (3, 0)));
+        for(int i = n - 1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    dp[i][1][cap] = max(-prices[i] + dp[i + 1][0][cap], 0 + dp[i + 1][1][cap]);
+                    dp[i][0][cap] = max(prices[i] + dp[i + 1][1][cap - 1], 0 + dp[i + 1][0][cap]);
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+};
