bab8b9fae87787c2eea937716f803e9f45686c98
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-08-05 10:23:41 +0530

    maximum subarray and remove project

---
+++ Patterns/graphs/removeProject.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    void dfsHelper(int node, vector<bool>& vis, vector<vector<int>>& adj, vector<int>& dfs){
+        vis[node] = true;
+        dfs.push_back(node);
+        for(int neighbour: adj[node]){
+            if(!vis[neighbour]){
+                dfsHelper(neighbour, vis, adj, dfs);
+            }
+        }
+    }
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<bool> visited(n, false);
+        vector<int> result;
+        vector<vector<int>> adj(n);
+
+        for (auto &edge : invocations) {
+            adj[edge[0]].push_back(edge[1]);
+        }   
+        dfsHelper(k, visited, adj, result);
+        for(auto &edge:invocations){
+            int u = edge[0];
+            int v = edge[1];
+            if(!visited[u] && visited[v]){
+                vector<int> ans;
+                for(int i = 0; i < n; i++){
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                ans.push_back(i);
+            }
+        }
+
+        return ans; 
+    }
+};
