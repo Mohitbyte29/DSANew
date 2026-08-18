48fb65391b228033b508143ef8cc1e6eb66f543a
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2026-08-18 19:40:26 +0530

    search rotated sorted array with duplicates

--- Patterns/binarySearch/searchRotatedArray.cpp
+++ Patterns/binarySearch/searchRotatedArray.cpp
@@ -58,3 +58,38 @@ int main() {
 
     return 0;
 }
+
+//* Contain Duplicates
+class Solution {
+public:
+    bool search(vector<int>& a, int target) {
+        int n = a.size();
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = high - (high - low)/2;
+            if(a[mid] == target) return true;
+            if (a[low] == a[mid] && a[mid] == a[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if(a[low] <= a[mid]){
+                if(target >= a[low] && target <= a[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else {
+                if(target <= a[high] && target >= a[mid]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
